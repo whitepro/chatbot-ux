1d2d18905382a155f0d4120f841dd51a6ec990ce
whitepro <48268425+whitepro@users.noreply.github.com>
2019-05-20 17:15:49 +0200

    Create jsondata.js

---
+++ public/js/services/jsondata.js
@@ -0,0 +1,32 @@
+var jsondata = {
+	"1": {
+			"question": "May I collect your activity preferences so the YMCA can learn your interests for future events?",
+	},
+	"2": {
+			"question": "Gender?",
+	},
+	"3": {
+			"question": "Which of the following YMCA activities would you like to participate in?",
+	},
+	"4": {
+			"question": "What days are you available to participate in YMCA activities?",
+	},
+	"5": {
+			"question": "What is your Zip Code?",
+	},
+	"6": {
+			"question": "How old are you?",
+	},
+	"7": {
+			"question": "How often would you like to receive activity reminders?",
+	},
+	"8": {
+			"question": "How far in advance would you like reminders?",
+	},
+	"weekQ1": {
+		"question": "Please press on a day to see events and activities happening near you.",
+	},
+	"gen_continue": {
+		"question": "Would you like to add additional events?",
+	}
+}
