5d1279ccf008a1fcb178f22ad28bc00b7f8773da
whitepro <48268425+whitepro@users.noreply.github.com>
2019-05-20 17:16:21 +0200

    Update events-demo.js

--- public/js/events-demo.js
+++ public/js/events-demo.js
@@ -1,12 +1,6 @@
 $(document).ready(function(){
-    $("#botQuestions").on("click", ".card", function(e){
-        const hidden = $(this).find(".success-overlay").hasClass("hide");
-        if(hidden){
-            M.toast({html: 'registered!', classes: 'green'});
-            return $(this).find(".success-overlay").removeClass("hide");
-        }
-        $(this).find(".success-overlay").addClass("hide");
-
+    $("#botQuestions").on("click", ".card-register", function(e){
+        M.toast({html: 'Registered!', classes: 'green'});
     });
 
     $("#botQuestions").on("click", ".btn-events-done", function(){
